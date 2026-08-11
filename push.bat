aeb014553d1b0b35ad67bb98977016dc6f3eb3b7
Dhanush Bhandary <dhanushbhandary88@gmail.com>
2026-08-11 11:51:37 +0530

    chore: integrate local build execution into deployment script and bypass Vercel build process

--- push.bat
+++ push.bat
@@ -1,8 +1,9 @@
 git config user.email "dhanushbhandary88@gmail.com"
 git config user.name "Dhanushbhandary"
 git init
+call npm run build
 git add .
-git commit -m "first commit"
+git commit -m "Update static build and deploy"
 git branch -M main
 git remote add origin https://github.com/DhanushbhandaryD123/GDPL.git
 git push -u origin main
